4678dc5c04f8f93c65fb69b7045b544b4220122e
dtran39 <trananhduc@gatech.edu>
2017-03-08 12:32:50 -0500

    Debugging tool: Improve your developer experience video 12:25

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
@@ -6,12 +7,17 @@ var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 // What the above code block does is : create an index.html file and inject itself
 // into the index_bundle.js file
+var PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist'),
+}
 module.exports = {
+  devtool: "cheap-module-source-map",
   entry: [
-    './app/index.js'
+    PATHS.app
   ],
   output: {
-    path: __dirname + '/dist',
+    path: PATHS.build,
     filename: 'index_bundle.js'
   },
   module: {
