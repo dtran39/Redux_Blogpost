fe5daec1525f2425aa9b3e84f915af167421f9b1
dtran39 <trananhduc@gatech.edu>
2017-03-13 16:45:53 -0400

    Finishing auth mockup, create Facebook auth (Auth end)

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,5 @@
+// ./ means folder the file are in, no dots mean start searching from the root
 export Home from './Home/Home';
 export Navigation from './Navigation/Navigation';
 export Authenticate from './Authenticate/Authenticate';
+export FacebookAuthButton from './FacebookAuthButton/FacebookAuthButton';
---
+++ app/helpers/auth.js
@@ -0,0 +1,11 @@
+export default function auth() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: 'Duc Tran',
+        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/11667298_967621593259350_9088687871227869340_n.jpg?oh=e02674e601069531c78cb9261a9ede5a&oe=5968B8DE',
+        uid: 'dtran39'
+      });
+    }, 2000);
+  });
+};
