2261db361aed7e1aec79f62d399aa77c0823d417
dtran39 <trananhduc@gatech.edu>
2017-03-13 15:53:27 -0400

    Add navbar with authentication link (Home route 13:12)

---
+++ app/components/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router'
+function NavLinks ({isAuthed}) {
+  return isAuthed === true
+    ? <ul>
+      <li><Link to='/'>{'Home'}</Link></li>
+    </ul>
+    : null
+}
+function ActionLinks ({isAuthed}) {
+  return isAuthed === true
+    ? <ul>
+      <li>New Post</li>
+      <li><Link to='/logout'>{'Logout'}</Link></li>
+    </ul>
+    : <ul>
+      <li><Link to='/'>{'Home'}</Link></li>
+      <li><Link to='/auth'>{'Authenticated'}</Link></li>
+    </ul>
+}
+export default function Navigation({isAuthed}) {
+  return (
+    <div>
+      <nav>
+        <NavLinks isAuthed={isAuthed}/>
+        <ActionLinks isAuthed={isAuthed}/>
+      </nav>
+    </div>
+  );
+}
--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {container, innerContainer} from './styles.css';
+import {Navigation} from 'components'
 const MainContainer = React.createClass({
   render () {
     return (
       <div className={container}>
+        <Navigation isAuthed={true} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
