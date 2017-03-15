fb920a5cdf37cf03543d34bb2c2df9fb62f11f5e
dtran39 <trananhduc@gatech.edu>
2017-03-15 11:02:04 -0400

    Create redux store (Connect to redux 8:30)

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
-
+import {createStore} from 'redux';
+import users from 'redux/modules/users'
+const store = createStore(users);
+console.log(store.getState());
 ReactDOM.render(routes, document.getElementById('app'))
