c8f46b5ae6d87d8cfb9fbff7be31c4f739cda2da
dtran39 <trananhduc@gatech.edu>
2017-03-20 20:46:33 -0400

    Adding firebase authentication functionality (firebase 9:00)

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,18 +1,16 @@
+import {ref, firebaseAuth} from 'config/constants'
 export default function auth() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        name: 'Duc Tran',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/11667298_967621593259350_9088687871227869340_n.jpg?oh=e02674e601069531c78cb9261a9ede5a&oe=5968B8DE',
-        uid: 'dtran39'
-      });
-    }, 2000);
-  });
+  return firebaseAuth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
 };
 export function checkIfAuthed(store) {
   // adding firebase later
   return store.getState().isAuthed
 }
 export function logout() {
-  console.log('Logged Out')
+  return firebaseAuth().signout()
+}
+export function saveUser(user) {
+  return ref.child(`users/${user.uid}`).
+    .set(user)
+    .then(() => user)
 }
